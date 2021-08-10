0a992b2a8c3e4b50dbc44320548dc9a384b0d351
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-08-09 23:43:31 -0700

    user and message models

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,18 @@
+const express = require("express");
+const router = new express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const ExpressError = require("../expressError");
+const db = require("../db");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
